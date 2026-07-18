18553c54fee5364d9711fb4c8dcce6bfe6ae2ee4
cxmbinh-ic <cxmbinh@gmail.com>
2026-07-18 15:42:15 +0700

    update interface for branch checking in scoreboard

--- cpu_if.sv
+++ cpu_if.sv
@@ -15,6 +15,12 @@ logic [31:0] monitor_mem_data;
 logic [31:0] monitor_imm;
 //for branch
 logic        monitor_branch;
+//for scoreboard checking branch
+logic        monitor_branch_taken;
+logic [31:0] monitor_id_pc;
+logic [31:0] monitor_id_pc_next;
+logic [31:0] monitor_mem_pc;
+logic [31:0] monitor_mem_pc_next;
 // decoded instruction fields
 logic [6:0]  monitor_opcode;
 logic [4:0]  monitor_rs1;
@@ -25,6 +31,8 @@ logic [6:0]  monitor_funct7;
 logic [1:0]  monitor_forward_A;
 logic [1:0]  monitor_forward_B;
 logic        monitor_PC_write;
+// for real time stall for SVA
+logic        Control_Mux_SVA;
 //clocking driver
 clocking cb_driver @(posedge clk);
 default input #1step output #1ns;
@@ -35,8 +43,8 @@ clocking cb_monitor @(posedge clk);
 default input #1step output #1ns;
 input monitor_pc, monitor_pc_next, monitor_instr, monitor_reg_write, monitor_rd,
       monitor_rd_data, monitor_mem_write, monitor_mem_addr, monitor_mem_data,
-      monitor_imm, monitor_branch,
-      monitor_opcode, monitor_rs1, monitor_rs2, monitor_funct3, monitor_funct7
-      ,monitor_forward_A, monitor_forward_B, monitor_PC_write;
+      monitor_imm, monitor_branch, monitor_opcode, monitor_rs1, monitor_rs2, monitor_funct3, monitor_funct7,
+      monitor_forward_A, monitor_forward_B, monitor_PC_write,
+      monitor_branch_taken, monitor_id_pc, monitor_id_pc_next, monitor_mem_pc, monitor_mem_pc_next ;
 endclocking
 endinterface
